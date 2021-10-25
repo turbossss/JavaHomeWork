00109c6d8290600b942da67aca8e5b5ce25088fe
Tkachenko Evgenij <evgenij.tkachenko@privatbank.ua>
2021-10-25 21:51:02 +0300

    Capital Letter

---
+++ src/com/pb/tkachenko/hw4/CapitalLetter.java
@@ -0,0 +1,28 @@
+package com.pb.tkachenko.hw4;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.lang.*;
+
+public class CapitalLetter {
+    static String makeUpper(String text){
+        String[] textWords = text.split(" ");
+        String result="";
+        for(int i=0;i<textWords.length;i++){
+            //
+            result+=textWords[i].substring(0,1).toUpperCase() + textWords[i].substring(1)+" ";
+        }
+        result=result.trim();
+        return result;
+    }
+
+    static void print(String text){
+        System.out.println(text);
+    }
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String text = scan.nextLine();
+
+        print(makeUpper(text));
+    }
+}
